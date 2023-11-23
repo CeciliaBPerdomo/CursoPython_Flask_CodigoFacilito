02d49acedc607d2b391c4bca51feb17665cc5beb
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2023-11-23 11:15:39 -0300

    Modulo 1

--- app/app.py
+++ app/app.py
@@ -1,11 +1,22 @@
 #pip list para chequear que tenemos instalado
 
-from flask import Flask, render_template     # Importar flask
+from flask import Flask, render_template, request     # Importar flask
 
 app = Flask(__name__)       # Creamos una variable para almacenar nuestra aplicacion mediante una instancia de Flask
 
+@app.before_request         # antes de la peticion
+def before_request():
+    print("Antes de la peticion")
+
+@app.after_request          # despues de la peticion
+def after_request(response):
+    print("Despues de la peticion")
+    return response
+
+
 @app.route('/')             # Ruta raiz de nuestra aplicacion
 def index():
+    print("Estamos realizando la peticion...")
     # return render_template("index.html", ) #Importar desde flask
     # return "Bienvenido a este curso de CodigoFacilito"
     data = {
@@ -50,7 +61,17 @@ def lenguajes():
         "lenguajes": ["PHP", "Python", "Kotlin", "Java", "C#", "JavaScript"]
     }
     return render_template('lenguajes.html', data=data)
- 
+
+# HTTP: HyperText Transfer Protocol
+# GET, POST, PUT, DELETE
+
+@app.route('/datos') #http://127.0.0.1:5005/datos?valor1=Python
+def datos(): #Query String
+    # print(request.args) --> Impresion de los argumentos de la peticion
+    valor = request.args.get('valor1')
+    valor2 = int(request.args.get('valor2')) #http://127.0.0.1:5005/datos?valor1=Python&valor2=28
+    return 'Estos son los datos: {0}, {1}'.format(valor, (valor2 + 15))
+
 if __name__ == '__main__':  # consultamos si es la principal para poder correr el servidor.
     #app.add_url_rule('/', view_func=index) #otra forma de ver la ruta raiz
     #app.run()              # Para correr el servidor en la terminal:
--- readme.md
+++ readme.md
@@ -1,11 +1,30 @@
+## Python, Flask y JinJa2
+
 #### Para inicializar el entorno virtual
 - virtualenv -p python3 env
 
 #### Para activar el entorno virtual: 
-./env/Scripts/activate
+- ./env/Scripts/activate
 
-#### Desactivar el entorno virual:
-desactivate
+#### Desactivar el entorno virtual:
+- ./env/Scripts/desactivate
 
 #### Para levantar el servidor: 
-python .\app\app.py
+- python .\app\app.py
+
+
+### Modulo 1 Fundamentos: 14 Clases: 
+- Introducción al curso
+- Conocimientos previos
+- ¿Qué es un framework?
+- Entorno virtual de desarrollo e instalación
+- Primer servidor con Flask
+- Primera página web y Hola mundo
+- Render de templates
+- Archivos estáticos
+- Herencia de plantillas
+- Hipervinculos (Links)
+- Dinamismo páginas web
+- Paso de parámetros mediante URL
+- Query String
+- Callbacks: Métodos antes y despúes de las peticiones
